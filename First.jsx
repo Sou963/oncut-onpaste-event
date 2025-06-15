09e4af2fdce9159c83a2f1f31840805c7676c088
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2025-06-15 14:55:10 +0600

    Add files via upload

---
+++ First.jsx
@@ -0,0 +1,11 @@
+import React ,{Component} from 'react'
+
+export default class Just extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Mother is best</h1>
+            </div>
+        )
+    }
+}
