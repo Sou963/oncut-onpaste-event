0456f98f98f8fe6f03482c98c2dac4104c19b2d1
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2025-07-18 23:39:05 +0600

    Add files via upload

---
+++ chat gpt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  char name[] = "Sourav";
+  int i;
+  for(i = 0; i < 6; i++) {
+    printf("%c", name[i]);
+  }
+  return 0;
+}
+
---
+++ cma.72.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+    int n,col,row;
+    printf("n=");
+    scanf("%d",&n);
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("%d",row*col);
+        }
+        printf("\n");
+    }
+    return 0;
+}
